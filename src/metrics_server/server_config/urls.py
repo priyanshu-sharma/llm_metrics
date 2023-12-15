e49524aedee1b3df17294f689aec7dbdbd065b89
priyanshu-sharma <prynsh1112@gmail.com>
2023-12-14 19:46:26 -0800

    basic configuration

---
+++ src/metrics_server/server_config/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for metrics_server project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.urls import path, include, re_path
+from server_config import health_check_view
+from rest_framework_swagger.views import get_swagger_view
+# from django.conf.urls.static import static
+
+
+schema_view = get_swagger_view(title='Metrics Server API')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path("^health_check$", health_check_view),
+    re_path('^docs', schema_view),
+]
