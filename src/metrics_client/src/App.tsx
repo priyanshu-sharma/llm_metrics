b17c9755134a71feca5f1717e41be2ba924917d4
priyanshu-sharma <prynsh1112@gmail.com>
2023-12-18 14:15:05 -0800

    added frontend setup

---
+++ src/metrics_client/src/App.tsx
@@ -0,0 +1,32 @@
+// import React from 'react';
+// import './App.css';
+
+import Title from "./components/Title/Title";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <div className="Title">
+//           <p className="Title-Heading">LLM Metrics UI</p>
+//         </div>
+//         <div className="Upload">
+//           <p>LLM Metrics UI</p>
+//         </div>
+//         <div className="Table">
+//           <p>LLM Metrics UI</p>
+//         </div>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+const App = () => {
+  return <div className="background">
+    {/* <Title /> */}
+  </div>
+}
+
+export default App;
---
+++ src/metrics_client/src/components/Title/Title.tsx
@@ -0,0 +1,11 @@
+import './Title.css';
+
+const Title = () => {
+  return (
+    <div className="Title">
+      <p className="Title-Heading">LLM Metrics UI</p>
+    </div>
+  );
+};
+
+export default Title
